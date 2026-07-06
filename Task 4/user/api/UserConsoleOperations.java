f4a18a4c10c838acd05cf8ce2be788a707614fb4
Exesting <zadnovaleksej@gmail.com>
2026-07-06 13:43:40 +0300

    Add files via upload

---
+++ Task 4/user/api/UserConsoleOperations.java
@@ -0,0 +1,113 @@
+package ru.itis.shop.user.api;
+
+import ru.itis.shop.user.application.UserService;
+
+import java.util.Scanner;
+import java.util.Optional;
+import ru.itis.shop.user.domain.User;
+public class UserConsoleOperations {
+
+    private final UserService userService;
+    private final Scanner scanner;
+
+    public UserConsoleOperations(UserService userService) {
+        this.userService = userService;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showMenu() {
+        printUserMenu();
+
+        String command = scanner.nextLine();
+
+        switch (command) {
+            case "1": {
+                signUp();
+            }
+            break;
+            case "2": {
+                signIn();
+            }
+            break;
+            case "3": {
+                findUserById();
+            }
+            break;
+            case "4": {
+                updateProfileDescription();
+            }
+            break;
+            case "5": {
+                findAll();
+            }
+            break;
+            case "0": {
+                System.exit(0);
+            }
+        }
+    }
+
+    private static void printUserMenu() {
+        System.out.println("1. Регистрация пользователя");
+        System.out.println("2. Вход в систему");
+        System.out.println("3. Найти пользователя по id");
+        System.out.println("4. Обновить описание профиля");
+        System.out.println("5. Показать всех пользователей");
+        System.out.println("0. Выход");
+    }
+
+    private void signUp() {
+        System.out.println("Сейчас будем регистрировать пользователя");
+        System.out.println("Введите email:");
+        String email = scanner.nextLine();
+        System.out.println("Введите password:");
+        String password = scanner.nextLine();
+        System.out.println("Введите описание профиля:");
+        String profileDescription = scanner.nextLine();
+
+        userService.signUp(email, password, profileDescription);
+    }
+
+
+    private void signIn() {
+        System.out.println("Вы можете войти в приложение");
+        System.out.println("Введите email:");
+        String email = scanner.nextLine();
+        System.out.println("Введите password:");
+        String password = scanner.nextLine();
+
+        if (userService.signIn(email, password)) {
+            System.out.println("Вы вошли в приложение");
+        } else {
+            System.out.println("Email или пароль не верны");
+        }
+    }
+
+    private void findUserById() {
+        System.out.println("Введите id:");
+        String id = scanner.nextLine();
+        Optional<User> userOptional = userService.findById(id);
+        if (userOptional.isPresent()) {
+            System.out.println("Пользователь найден:");
+            System.out.println(userOptional.get());
+        } else {
+            System.out.println("Пользователь не найден.");
+        }
+    }
+
+    private void updateProfileDescription() {
+        System.out.println("Введите email пользователя:");
+        String email = scanner.nextLine();
+        System.out.println("Введите новое описание профиля:");
+        String description = scanner.nextLine();
+        if (userService.updateProfileDescription(email, description)) {
+            System.out.println("Описание успешно обновлено.");
+        } else {
+            System.out.println("Пользователь не найден.");
+        }
+    }
+
+    public void findAll() {
+        userService.findAll();
+    }
+}
